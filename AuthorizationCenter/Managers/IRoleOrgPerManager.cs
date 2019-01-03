de1136aaf2ee93881c0304af215dabe618f42cf8
wangsen <wagsn@foxmail.com>
2019-01-03 13:55:23 +0800

    商铺草稿-单个删除-响应体

---
+++ AuthorizationCenter/Managers/IRoleOrgPerManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationCenter.Managers
+{
+    /// <summary>
+    /// 角色组织权限关联管理
+    /// </summary>
+    public interface IRoleOrgPerManager<TJson>
+    {
+        /// <summary>
+        /// 某用户是否具有某权限
+        /// </summary>
+        /// <param name="userId">用户ID</param>
+        /// <param name="orgId">组织ID</param>
+        /// <param name="perId">权限ID</param>
+        /// <returns></returns>
+        Task<bool> HasPermission(string userId, string orgId, string perId);
+
+        /// <summary>
+        /// 查询角色的权限
+        /// </summary>
+        /// <param name="roleId">角色ID</param>
+        /// <returns></returns>
+        IQueryable<TJson> FindByRoleId(string roleId);
+
+        /// <summary>
+        /// 查询一个用户的权限
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        IQueryable<TJson> FindByUserId(string id);
+
+        /// <summary>
+        /// 查询通过ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        IQueryable<TJson> FindById(string id);
+
+        /// <summary>
+        /// 删除通过ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        IQueryable<TJson> DeleteById(string id);
+    }
+}
---
+++ AuthorizationCenter/Stores/IRoleOrgPerStore.cs
@@ -0,0 +1,16 @@
+﻿using AuthorizationCenter.Entitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationCenter.Stores
+{
+    /// <summary>
+    /// 角色组织权限关联存储接口
+    /// </summary>
+    public interface IRoleOrgPerStore: IStore<RoleOrgPer>
+    {
+
+    }
+}
